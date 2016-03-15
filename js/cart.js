41d6e4ada350499796309e35b81b18d256343892
JeffWu <isjeffcom@gmail.com>
2016-03-15 12:50:33 +0000

    Added files via upload

--- js/cart.js
+++ js/cart.js
@@ -10,8 +10,20 @@
 
 
 
-//Main Function 
+//jQuery Function 
 (function($){
+
+	/*Interface Explanation
+		 *names: All item in cart array (no '[]')
+		 *cartAll: All item in cart array (with '[]')
+		 *pprice: All price array (no '[]')
+		 *priceAll: All price array (with '[]')
+		 *TPrice: Total price (calculate all price)
+		 *qty: Item quantity array (no '[]')
+		 *mqtyAll: Item quantity array (with '[]')
+		 *mQty: User input quantity for THIS (current select) item (only use in add function)
+		 *names.length: get number > how many item in cart
+	*/
 
     //DOM ready for all
 	$(document).ready(function(){
@@ -28,14 +40,22 @@
         var priceAll = localStorage.getItem('price');
         
         var totalPrice = 0;
+		var TPrice = 0;
+		var times= 0;
+		var tpprice;
 		
+		var btnNum;
+		var btnRealNum;
+		var removeBtn;
+		var bottonValue;
+		
+
+
 		/*ADD BY Andreea.Molnar
 		 * for fix the Items array
 		 * empty after refresh
 		 * 2016/03/08*/
 		 
-
-		
 		//Get value from localStorage
 		if (cartAll != null) {
 			//Count Products Number
@@ -54,10 +74,6 @@
 				var mName = $(this).parent().children('.name').html();
 				var mPrice = $(this).parent().children('.price').html();
 				var mQty = $(this).parent().children('.qty').val();
-				
-				console.log(mName);
-				console.log(mPrice);
-				console.log(mQty);
 				// Change type of data to integer forcibly
 				mQty = parseInt(mQty);  
 				
@@ -66,14 +82,13 @@
 					for(var i=0; i<names.length;i++){
 						if(mName == names[i]){
 							alert('Item repeat');
-							console.log(names);
 							return;
 						}
 					}
 				}
 
 				//Calculate the price with quantity
-				if(mQty >=0 ){
+				if(mQty >= 1 &&  mQty <= 100){
 
 					var mPriceCal = mPrice * mQty;
 					mPriceCal = parseFloat(mPriceCal).toFixed(2);  //Fix the calculate issue.
@@ -81,16 +96,14 @@
 				}else{
 							
 					//verity input number
-					alert("Please input a right number");
-					mQty = 1;
-					mPriceCal = mPrice * mQty;
+					alert("Please input a right number (1-100)");
+					return;
+					//do nothing...
 					
 				};
 				
 				//Add item to HTML localStorage 
 								
-				
-				
 				qty.push(mQty);
 				localStorage.setItem ('mQty', JSON.stringify(qty));
 				
@@ -104,53 +117,83 @@
 				localStorage.setItem ('price', JSON.stringify(pprice));
 
 				//Alert to costumer
-				alert("You Add " + mName + " in cart successful " + " Price: " + mPrice + "  You already have ");
+				alert("You Add " + mName + " in cart successful " + "Quantity: " + mQty + " Price: " + mPriceCal);
+				
+				$(".cartItem").html("Cart Product: " + names);
+				
+				//Calculate Total price 
+				tpprice = pprice;
 
+				for(var i = times; i < tpprice.length; i++){
+					totalPrice += parseFloat(tpprice[i]);
+					TPrice = totalPrice.toFixed(2);
+				}	
+				times++;
+				
+				//Display Total Price
+				$(".totalPrice").html("All Price: " + TPrice + " $ ");
+				
+							
+				if(names !== null){
+				   
+					//Button create function
+					//Clear exist button
+					$(".removeBtn").remove();
+					
+					//Create child
+					for(btnNum = 0; btnNum < names.length ; btnNum++){
+							btnRealNum = btnNum + 1;	//Display real item number
+							removeBtn = document.createElement('button');    //Create Button
+							removeBtn.className = "removeBtn";  //Add className
+							bottonValue = document.createTextNode('Item' + btnRealNum);  //Give Button Value
+							removeBtn.appendChild(bottonValue);  //Add Child
+							document.body.appendChild(removeBtn);  
+							}
+
+				}else{
+					
+						//do nothing
+
+				}
+				
 		});
 		
 		
 		//Display Cart Function
 		$(function(){
 		
-		    //Cal Total price  
-		    var TPrice;
+		    //Cal Total price Secound fix  
+
 			if(names[0] !== undefined){
-				console.log('price calculate info ' + pprice + priceAll);
-	            var ppprice = JSON.parse(priceAll);
-	            console.log(names[0]);
+
+	            tpprice = pprice;
 	
-				for(var i = 0; i < ppprice.length; i++){
-					totalPrice += parseFloat(ppprice[i]);
+				for(var i = 0; i < tpprice.length; i++){
+					totalPrice += parseFloat(tpprice[i]);
 					TPrice = totalPrice.toFixed(2);
 					}	
-		
 			}else{
 				
 				//if No item...
 				TPrice = 0;
-				
 		    }
 			
-
 			//Display Cart 2Front
 	        
-			if(cartAll != null){
+			if(names != null){
 			
-				$(".cartItem").html("Cart Product: " + cartAll);
-				$(".totalPrice").html("All Price: " + TPrice + " Pound ");
+				$(".cartItem").html("Cart Product: " + names);
+				$(".totalPrice").html("All Price: " + TPrice + " $ ");
 
 			}
 		
-
-			
 			$('.updateCart').click(function(){
 			console.log('Start UpdateCart // refresh the page');
 			
-				location.reload(true)
+				location.reload(true);
 			
 			});
-		
-
+			
 			//Clear Cart
 
 			
@@ -158,45 +201,40 @@
 				
 				alert("Clear Cart");
 				localStorage.clear();
+				location.reload(true);
 				
 			});
 			
-			
-			
-			
-			//Remove item from Cart
-
-			if(JSON.parse(priceAll) !== null){
-           
-			
-			    //Button create function
-				for(var btnNum = 0; btnNum < JSON.parse(cartAll).length; btnNum++){
-				
-					var btnRealNum = btnNum + 1;	//Display real item number
-					var removeBtn = document.createElement('button');    //Create Button
-					removeBtn.className = "removeBtn";  //Add className
-					var bottonValue = document.createTextNode('Remove Item' + btnRealNum);  //Give Button Value
-					removeBtn.appendChild(bottonValue);  //Add Child
-					document.body.appendChild(removeBtn);  
+			if(names !== null){
+				   
+					//Button create function
+					$(".removeBtn").remove();
 					
-					}
+					for(btnNum = 0; btnNum < names.length ; btnNum++){
+							btnRealNum = btnNum + 1;	//Display real item number
+							removeBtn = document.createElement('button');    //Create Button
+							removeBtn.className = "removeBtn";  //Add className
+							bottonValue = document.createTextNode('Remove Item' + btnRealNum);  //Give Button Value
+							removeBtn.appendChild(bottonValue);  //Add Child
+							document.body.appendChild(removeBtn);  
+							}
 
-			}else{
-			
-			    //return null
-				
-			    }
-				
+				}else{
+					
+						//do nothing
 
-			
-			
+				}
+
 			//remove item function
-			console.log(names);
+
 			$(".removeBtn").click(function(){
 			    
 				//Restore array number from button
 			    var thisNum = $(this).html();
 				thisNum = thisNum.slice(-1) -1;
+				
+				//Display which item has been remove
+				alert("Successful remove item : " + names[thisNum] + " from your shopping cart");
 				
 				//Delete element from array
 				names.splice(thisNum,1);
@@ -204,24 +242,18 @@
 				price.splice(thisNum,1);
 				qty.splice(thisNum,1);
 				
-				
 				//Update localStorage after Delete
 				localStorage.setItem ('price', JSON.stringify(pprice));
 				localStorage.setItem ('mPrice', JSON.stringify(price));
 				localStorage.setItem ('mQty', JSON.stringify(qty));
 				localStorage.setItem ('name', JSON.stringify(names));
 				
-				console.log(names);
+				//Update Page
+				location.reload(true);
 			});
-
-		
-		});
-		
-
-        
 			
+		});
 
-		
 		//Check if localStorage oException
 		try {
 		    localStorage.setItem(key, value);
